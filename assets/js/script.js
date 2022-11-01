eb92bafb7d4fa19ae2cd8364ce6a965d0733ab40
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2022-11-01 13:44:53 -0400

    Cleaned up code formatting, added local storage functionality, began working on displaying it on refresh.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,6 @@ var threePmBlock = document.querySelector("#block3pm")
 var fourPmBlock = document.querySelector("#block4pm")
 var fivePmBlock = document.querySelector("#block5pm")
 
-var nineamText = $("#nineAmText")
-var toDo = $(nineamText.value)
 
 hours=[9, 10, 11, 12, 13, 14, 15, 16, 17]
 var container = document.querySelector(".container")
@@ -33,71 +31,106 @@ if (currentDay.format("HH") == 9) {
       else if (currentDay.format("HH") < 9){
             nineAmBlock.classList.add("future")
         }
+        
+if (currentDay.format("HH") == 10) {
+    tenAmBlock.classList.add("present");
+    } else if (currentDay.format("HH") > 10) {
+            tenAmBlock.classList.add("past")
+        }
+        else if (currentDay.format("HH") < 10){
+            tenAmBlock.classList.add("future")
+        }
 
-        if (currentDay.format("HH") == 10) {
-            tenAmBlock.classList.add("present");
-            } else if (currentDay.format("HH") > 10) {
-                    tenAmBlock.classList.add("past")
-                }
-              else if (currentDay.format("HH") < 10){
-                    tenAmBlock.classList.add("future")
-                }
-                if (currentDay.format("HH") == 11) {
-                    elevenAmBlock.classList.add("present");
-                    } else if (currentDay.format("HH") > 11) {
-                            elevenAmBlock.classList.add("past")
-                        }
-                      else if (currentDay.format("HH") < 11){
-                            elevenAmBlock.classList.add("future")
-                        }
+if (currentDay.format("HH") == 11) {
+    elevenAmBlock.classList.add("present");
+    } else if (currentDay.format("HH") > 11) {
+            elevenAmBlock.classList.add("past")
+        }
+        else if (currentDay.format("HH") < 11){
+            elevenAmBlock.classList.add("future")
+        }
                         
-        if (currentDay.format("HH") == 12) {
-            noonBlock.classList.add("present");
-            } else if (currentDay.format("HH") > 12) {
-                    noonBlock.classList.add("past")
-                }
-              else if (currentDay.format("HH") < 12){
-                    noonBlock.classList.add("future")
-                }
-                if (currentDay.format("HH") == 13) {
-                    onePmBlock.classList.add("present");
-                    } else if (currentDay.format("HH") > 13) {
-                            onePmBlock.classList.add("past")
-                        }
-                      else if (currentDay.format("HH") < 13){
-                            onePmBlock.classList.add("future")
-                        }
-                        if (currentDay.format("HH") == 14) {
-                            twoPmBlock.classList.add("present");
-                            } else if (currentDay.format("HH") > 14) {
-                                    twoPmBlock.classList.add("past")
-                                }
-                              else if (currentDay.format("HH") < 14){
-                                    twoPmBlock.classList.add("future")
-                                }
-                                if (currentDay.format("HH") == 15) {
-                                    threePmBlock.classList.add("present");
-                                    } else if (currentDay.format("HH") > 15) {
-                                            threePmBlock.classList.add("past")
-                                        }
-                                      else if (currentDay.format("HH") < 15){
-                                            threePmBlock.classList.add("future")
-                                        }
+if (currentDay.format("HH") == 12) {
+    noonBlock.classList.add("present");
+    } else if (currentDay.format("HH") > 12) {
+            noonBlock.classList.add("past")
+        }
+        else if (currentDay.format("HH") < 12){
+            noonBlock.classList.add("future")
+        }
+
+if (currentDay.format("HH") == 13) {
+    onePmBlock.classList.add("present");
+    } else if (currentDay.format("HH") > 13) {
+            onePmBlock.classList.add("past")
+        }
+        else if (currentDay.format("HH") < 13){
+            onePmBlock.classList.add("future")
+        }
+
+if (currentDay.format("HH") == 14) {
+    twoPmBlock.classList.add("present");
+    } else if (currentDay.format("HH") > 14) {
+            twoPmBlock.classList.add("past")
+        }
+        else if (currentDay.format("HH") < 14){
+            twoPmBlock.classList.add("future")
+        }
+
+if (currentDay.format("HH") == 15) {
+    threePmBlock.classList.add("present");
+    } else if (currentDay.format("HH") > 15) {
+            threePmBlock.classList.add("past")
+        }
+        else if (currentDay.format("HH") < 15){
+            threePmBlock.classList.add("future")
+        }
     
-                                        if (currentDay.format("HH") == 16) {
-                                            fourPmBlock.classList.add("present");
-                                            } else if (currentDay.format("HH") > 16) {
-                                                    fourPmBlock.classList.add("past")
-                                                }
-                                              else if (currentDay.format("HH") < 16){
-                                                    fourPmBlock.classList.add("future")
-                                                }
-                                                if (currentDay.format("HH") == 17) {
-                                                    fivePmBlock.classList.add("present");
-                                                    } else if (currentDay.format("HH") > 17) {
-                                                            threePmBlock.classList.add("past")
-                                                        }
-                                                      else if (currentDay.format("HH") < 17){
-                                                            fivePmBlock.classList.add("future")
-                                                        }
-                    
+if (currentDay.format("HH") == 16) {
+    fourPmBlock.classList.add("present");
+    } else if (currentDay.format("HH") > 16) {
+            fourPmBlock.classList.add("past")
+        }
+        else if (currentDay.format("HH") < 16){
+            fourPmBlock.classList.add("future")
+        }
+        
+if (currentDay.format("HH") == 17) {
+    fivePmBlock.classList.add("present");
+    } else if (currentDay.format("HH") > 17) {
+            threePmBlock.classList.add("past")
+        }
+        else if (currentDay.format("HH") < 17){
+            fivePmBlock.classList.add("future")
+        }
+
+
+function saveTimeBlock(){
+    var scheduleText = $(this).siblings("textarea").eq(0).val()
+    var time = $(this).siblings(".hour").eq(0).text()
+    
+    var schedule = JSON.parse(localStorage.getItem("schedule"));
+
+    if (schedule === null){
+        schedule = {};
+    }
+
+    schedule[time] = scheduleText;
+    console.log(schedule);
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+function loadTimeBlock(){
+    var schedule = JSON.parse(localStorage.getItem("schedule"));
+    if (schedule === null){
+        return;
+    }
+    console.log(schedule);
+    var textarea = document.querySelector(".description")
+    textarea.value = JSON.stringify(schedule);
+}
+
+$(function(){
+    loadTimeBlock();
+    $(".saveBtn").on("click", saveTimeBlock);
+})
